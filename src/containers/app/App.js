cd3d8b9905fd72ccf7cfb97fb514ffc8cf7dc73b
Patrice <patrice.parny@airfinetworks.com>
2017-08-06 00:45:03 +0100

    Update stucture and adding eslint

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from '../../images/logo.svg';
 import './App.css';
+
+import H1 from '../../components/ui/H1';
 
 class App extends Component {
   render() {
@@ -8,7 +10,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <H1>coco</H1>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
--- styleguide.config.js
+++ styleguide.config.js
@@ -1,26 +1,41 @@
+'use strict';
+
+const path = require('path');
+
 module.exports = {
-  sections: [
-    {
-      name: 'Introduction',
-      content: 'src/documentation/introduction.md'
-    },
-    {
-      name: 'Documentation',
-      sections: [
-        {
-          name: 'Installation',
-          content: 'src/documentation/installation.md'
-        },
-        {
-          name: 'Configuration',
-          content: 'src/documentation/configuration.md'
-        }
-      ]
-    },
-    {
-      name: 'UI Components',
-      content: 'src/documentation/ui.md',
-      components: 'src/components/ui/**/*/*.js'
-    }
-  ]
+	require: [
+		path.join(__dirname, 'src/index.css'),
+	],
+	sections: [
+		{
+			name: 'Introduction',
+			content: 'src/documentation/introduction.md'
+		},
+		{
+			name: 'Documentation',
+			sections: [
+				{
+					name: 'Installation',
+					content: 'src/documentation/installation.md'
+				},
+				{
+					name: 'Configuration',
+					content: 'src/documentation/configuration.md'
+				}
+			]
+		},
+		{
+			name: 'UI Components',
+			content: 'src/documentation/ui.md',
+			components: 'src/components/ui/**/*/*.js'
+		}
+	],
+
+	template: 'src/documentation/styleguide.html',
+	theme: {
+		fontFamily: {
+			base: '"Ubuntu", sans-serif',
+		},
+	},
+	showUsage: false,
 };
