ef62e9ac772efefff005258f45ddbd9896ef56c4
Patrice <patrice.parny@airfinetworks.com>
2017-08-13 19:00:51 +0100

    Adding H2 <Component

---
+++ src/components/ui/H2/index.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import theme from '../../../styles/theme';
+
+const { fontSize } = theme;
+
+const H2 = styled.h2`font-size: ${fontSize.large};`;
+
+export default H2;
