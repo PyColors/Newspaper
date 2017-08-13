86846605a5a1bcab62014400147c95e1e3a1cd66
Patrice <patrice.parny@airfinetworks.com>
2017-08-13 18:52:29 +0100

    Added global theme add delee

---
+++ src/styles/theme.js
@@ -0,0 +1,42 @@
+/*
+ * Main Theme
+ *
+ * This contains all the styles for the App
+ */
+
+export const theme = {
+  // ******************
+  // Colors
+  // ******************
+  colors: {
+    primary: '',
+    secondary: '',
+    white: '#FFF',
+    black: '#222',
+    border: 'rgba(0,0,0,0.1)',
+    base: 'rgba(0,0,0,0.4)',
+    alert: '',
+    success: '',
+    info: '',
+    link: ''
+  },
+
+  // ******************
+  // Font-Size
+  // ******************
+  fontSize: {
+    xsmall: '9px',
+    small: '11px',
+    base: '13px',
+    large: '20px',
+    xlarge: '30px',
+    xxlarge: '50px'
+  },
+
+  // ******************
+  // Font-Family
+  // ******************
+  fontFamily: {}
+};
+
+export default theme;
