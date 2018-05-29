a6bdd3ed826cc091b511c05dd990f1635689c55b
Patrice <patweb44@gmail.com>
2018-05-29 23:04:27 +0000

    Adding Redux Provider, store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './containers/app/App';
+import configureStore from './store';
 import registerServiceWorker from './registerServiceWorker';
 
 import { IntlProvider } from 'react-intl';
 import WrapperApp from './containers/app/WrapperApp';
 
+const store = configureStore();
+
 ReactDOM.render(
   <IntlProvider locale="en">
     <WrapperApp>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </WrapperApp>
   </IntlProvider>,
   document.getElementById('root')
