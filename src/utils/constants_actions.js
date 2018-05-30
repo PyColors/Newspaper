018c0f7b68051da60f2160df9863f67e31efd231
Patrice <patweb44@gmail.com>
2018-05-30 20:30:39 +0000

    Adding constants actions file for global actions

---
+++ src/utils/constants_actions.js
@@ -0,0 +1,9 @@
+/* ACTIONS */
+
+/* Common */
+export const CLEAR_STORE = 'CLEAR_STORE';
+
+/* Home Page */
+export const FETCH_DATA = 'FETCH_DATA';
+export const RECEIVE_DATA = 'RECEIVE_DATA';
+export const FAILED_RECIEVE_DATA = 'FAILED_RECIEVE_DATA';
