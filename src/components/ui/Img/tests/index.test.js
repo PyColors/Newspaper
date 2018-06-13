ffb3778afc2912e8ed29932acef1167d1a10a19e
Patrice <patweb44@gmail.com>
2018-06-13 22:16:57 +0000

    Adding Test to Img <Component...

---
+++ src/components/ui/Img/tests/index.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Img from '../index';
+
+const src = 'src.png';
+const alt = 'description';
+
+const renderComponent = (props = {}) =>
+  shallow(<img src={src} alt={alt} {...props} />);
+
+describe('<Img />', () => {
+  it('should have an src attribute', () => {
+    const renderedComponent = renderComponent();
+    expect(renderedComponent.prop('src')).toEqual(src);
+  });
+
+  it('should have an alt attribute', () => {
+    const renderedComponent = renderComponent();
+    expect(renderedComponent.prop('alt')).toEqual(alt);
+  });
+
+  it('should render its text', () => {
+    const children = 'Text';
+    const renderedComponent = shallow(<Img>{children}</Img>);
+    expect(renderedComponent.contains(children)).toBe(false);
+  });
+
+  it('should not have a className attribute', () => {
+    const renderedComponent = renderComponent();
+    expect(renderedComponent.prop('className')).toBeUndefined();
+  });
+
+  it('should assume a className attribute', () => {
+    const className = 'test';
+    const renderedComponent = renderComponent({ className });
+    expect(renderedComponent.hasClass(className)).toBe(true);
+  });
+});
