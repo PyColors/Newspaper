fa2c584d5beee706479460cde653e012d55836b3
PyColors <patweb44@gmail.com>
2018-10-07 23:29:58 +0100

    Setting testing to the store

---
+++ src/tests/index.js
@@ -0,0 +1,25 @@
+import { browserHistory } from 'react-router-dom';
+import configureStore from '../store';
+
+describe('configureStore', () => {
+  let store;
+
+  beforeAll(() => {
+    store = configureStore({}, browserHistory);
+  });
+
+  describe('injectedReducers', () => {
+    it('should contain an object for reducers', () => {
+      expect(typeof store.injectedReducers).toBe('object');
+    });
+  });
+});
+
+describe('configureStore params', () => {
+  it('should call window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__', () => {
+    const compose = jest.fn();
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ = () => compose;
+    configureStore(undefined, browserHistory);
+    expect(compose).toHaveBeenCalled();
+  });
+});
