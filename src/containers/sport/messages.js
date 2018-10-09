38e8debe9557855565d1c9f9525c152d6bc91bff
PyColors <patweb44@gmail.com>
2018-10-09 21:38:24 +0100

    Implementing sport content

---
+++ src/containers/sport/messages.js
@@ -0,0 +1,12 @@
+/*
+ * Sport Messages
+ * This contains all the text for Sport container.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  titleMessage: {
+    id: 'newspaper.containers.App.title.message',
+    defaultMessage: '❝ Sport ❞'
+  }
+});
