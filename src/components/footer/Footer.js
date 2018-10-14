6385bd277cd5a2562df446bb9d02180e08603592
PyColors <patweb44@gmail.com>
2018-10-14 17:28:32 +0100

    Implementing styled-icons

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,22 @@
+import styled from 'styled-components';
 import React from 'react';
+import { LogoGithub } from 'styled-icons/octicons/LogoGithub';
+import theme from '../../styles/theme';
 
-const Footer = () => <p>Made with ♥ by Py Colors</p>;
+const { colors } = theme;
+
+const LogoGithubWhite = styled(LogoGithub)`
+  color: ${colors.white};
+`;
+
+const Footer = () => (
+  <footer>
+    <p>Made with ♥ by Py Colors</p>
+    <a href="https://github.com/PyColors/Newspaper">
+      {' '}
+      <LogoGithubWhite size="48" title="Logo Github" />{' '}
+    </a>
+  </footer>
+);
 
 export default Footer;
