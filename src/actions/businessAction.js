6c5e6b369a9a29cb0635954faa3369a56d75ac9e
PyColors <patweb44@gmail.com>
2018-10-17 23:04:50 +0100

    Implementing Business action

---
+++ src/actions/businessAction.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import * as constantsActions from '../utils/constants_actions';
+
+export function fetchDataIfNeeded() {
+  return (dispatch, getState) => {
+    if (shouldFetchData(getState())) {
+      return dispatch(fetchData());
+    }
+  };
+}
+
+export function fetchData() {
+  return dispatch => {
+    dispatch(beginFetchData());
+    axios
+      .get(
+        `https://newsapi.org/v2/top-headlines?country=fr&category=business&apiKey=${
+          process.env.REACT_APP_NEWS_API_KEY
+        }`
+      )
+      .then(res => {
+        dispatch(loadData(res));
+      })
+      .catch(error => {
+        const response = error.response;
+
+        if (response === undefined) {
+          dispatch(fetchFailure(error));
+        }
+      });
+  };
+}
+
+function shouldFetchData(state) {
+  if (state === null) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+const beginFetchData = () => ({
+  type: constantsActions.FETCH_DATA
+});
+
+const loadData = payload => ({
+  type: constantsActions.RECEIVE_DATA,
+  data: payload.data
+});
+
+const fetchFailure = () => ({
+  type: constantsActions.FAILED_RECIEVE_DATA,
+  error: true
+});
