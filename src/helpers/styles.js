699d723849a63c45c3562fb2e4db9330bea0a4e0
PyColors <patweb44@gmail.com>
2018-10-22 21:58:11 +0100

    Adding helper styles for common CSS

---
+++ src/helpers/styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const Global = styled.div`
+  width: 75%;
+  margin: 0 auto;
+`;
+
+export const Content = styled.div`
+  display: inline-block;
+  vertical-align: top;
+  width: 75%;
+  padding: 0;
+`;
+
+export const Article = styled.article`
+  max-width: 64rem;
+  padding: 1rem 3rem;
+  margin: 0 auto;
+  font-size: 1.1rem;
+`;
+
+export const Hr = styled.hr`
+  height: 1px;
+  padding: 0;
+  margin: 1rem 0;
+  background-color: #dcdbdb;
+  border: 0;
+`;
