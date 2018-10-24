bab393cf757dc7f01c765157010962c90f3fcc33
PyColors <patweb44@gmail.com>
2018-10-24 21:03:51 +0100

    Remove Helper test file

--- src/helpers/tests/index.test.js
+++
@@ -1,9 +0,0 @@
-import Hr from '../styles';
-
-describe('<Hr />', () => {
-  it('should render a prop', () => {
-    const id = 'HrId';
-    const renderedComponent = shallow(<Hr id={id} />);
-    expect(renderedComponent.prop('id')).toEqual(id);
-  });
-});
